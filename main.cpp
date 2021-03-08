d1e533253ccb7d2420a69c13b34ae06304410544
supermegaukulele <findamy@naver.com>
2021-03-08 17:06:06 +0100

    made blur demo

--- main.cpp
+++ main.cpp
@@ -7,83 +7,81 @@
 using namespace std;
 using namespace cv;
 
-int DELAY_CAPTION = 1500;
-int DELAY_BLUR = 100;
-int MAX_KERNEL_LENGTH = 31;
+int BLUR_TYPE = 0;
+int MAX_TYPE = 3;
+int BLUR_VALUE = 0;
+int MAX_VALUE = 100;
 
 Mat src;
 Mat dst;
-char window_name[] = "smoothing demo";
+
+const char *window_name = "Blur Demo";
+const char *trackbar_type = "Type: \n 0: Homogeneous \n 1: Gaussian \n 2: median_blur \n 3: Bilateral_Blur";
+const char *trackbar_value = "Value";
 
 int display_dst(const char *caption);
+
+static void Blur_Demo(int, void *)
+{
+    dst = src.clone();
+
+    if (BLUR_TYPE == 0)
+    {
+        for (int i = 1; i < BLUR_VALUE; i = i + 2)
+        {
+            blur(src, dst, Size(i, i), Point(-1, -1));
+        }
+    }
+
+    if (BLUR_TYPE == 1)
+    {
+        for (int i = 1; i < BLUR_VALUE; i = i + 2)
+        {
+            GaussianBlur(src, dst, Size(i, i), 0, 0);
+        }
+    }
+    if (BLUR_TYPE == 2)
+    {
+        for (int i = 1; i < BLUR_VALUE; i = i + 2)
+        {
+            medianBlur(src, dst, i);
+        }
+    }
+    if (BLUR_TYPE == 3)
+    {
+        for (int i = 1; i < BLUR_VALUE; i = i + 2)
+        {
+            bilateralFilter(src, dst, i, i * 2, i / 2);
+        }
+    }
+    imshow(window_name, dst);
+}
 
 int main(int argc, char **argv)
 {
     namedWindow(window_name, WINDOW_AUTOSIZE);
+    createTrackbar(trackbar_type, window_name, &BLUR_TYPE, MAX_TYPE, Blur_Demo);
+    createTrackbar(trackbar_value, window_name, &BLUR_VALUE, MAX_VALUE, Blur_Demo);
 
     const char *filename = argc >= 2 ? argv[1] : "lena.jpg";
-    
-    src = imread(samples::findFile(filename), IMREAD_COLOR);
+
+    src = imread(samples::findFile(filename), IMREAD_COLOR); //Load Image
     if (src.empty())
     {
         printf(" Error opening image\n");
         printf(" Usage:\n %s [image_name-- default lena.jpg] \n", argv[0]);
         return EXIT_FAILURE;
     }
-
-    dst = src.clone();
-
-    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2)
-    {
-        blur(src, dst, Size(i, i), Point(-1, -1));
-        if (display_dst("homogeneous blur") != 0)
-        {
-            return 0;
-        }
-    }
-    /*
-
-    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2)
-    {
-        GaussianBlur(src, dst, Size(i, i), 0, 0);
-        if (display_dst("Gaussian_Blur") != 0)
-        {
-            return 0;
-        }
-    }
-    
-
-    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2)
-    {
-        medianBlur(src, dst, i);
-        if (display_dst("median_blur") != 0)
-        {
-            return 0;
-        }
-    }
-
-    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2)
-    {
-        bilateralFilter(src, dst, i, i * 2, i / 2);
-        if (display_dst("Bilateral_Blur") != 0)
-        {
-            return 0;
-        }
-    } */
+    Blur_Demo(0, 0);
+    waitKey();
 
     return 0;
 }
 
-int display_dst(const char *caption)
-{
-    string filename = caption;
-    filename = filename + ".jpg";
-    imwrite(filename, dst);
-    imshow(window_name, dst);
-    int c = waitKey(1000);
-    if (c >= 0)
-    {
-        return -1;
-    }
-    return 0;
-}
+// int display_dst(const char *caption)
+// {
+//     string filename = caption;
+//     filename = filename + ".jpg";
+//     imwrite(filename, dst);
+//     return 0;
+// }
