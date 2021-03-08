05896b2494889a6f0f33bfaed967e48c804f887c
supermegaukulele <findamy@naver.com>
2021-03-08 10:19:13 +0100

    image blur

--- main.cpp
+++ main.cpp
@@ -1,17 +1,78 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
 
 using namespace std;
+using namespace cv;
 
-int main()
+int DELAY_CAPTION = 1500;
+int DELAY_BLUR = 100;
+int MAX_KERNEL_LENGTH = 31;
+
+Mat src;
+Mat dst;
+
+int display_dst(const char *caption);
+
+int main(int argc, char **argv)
 {
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string &word : msg)
+    const char *filename = argc >= 2 ? argv[1] : "lena.jpg";
+    src = imread(samples::findFile(filename), IMREAD_COLOR);
+    if (src.empty())
     {
-        cout << word << " ";
+        printf(" Error opening image\n");
+        printf(" Usage:\n %s [image_name-- default lena.jpg] \n", argv[0]);
+        return EXIT_FAILURE;
     }
-    cout << endl;
 
+    dst = src.clone();
+
+    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2)
+    {
+        blur(src, dst, Size(i, i), Point(-1, -1));
+        if (display_dst("homogeneous blur") != 0)
+        {
+            return 0;
+        }
+    }
+    /*
+
+    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2)
+    {
+        GaussianBlur(src, dst, Size(i, i), 0, 0);
+        if (display_dst("Gaussian_Blur") != 0)
+        {
+            return 0;
+        }
+    }
+    
+
+    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2)
+    {
+        medianBlur(src, dst, i);
+        if (display_dst("median_blur") != 0)
+        {
+            return 0;
+        }
+    }
+
+    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2)
+    {
+        bilateralFilter(src, dst, i, i * 2, i / 2);
+        if (display_dst("Bilateral_Blur") != 0)
+        {
+            return 0;
+        }
+    } */
+
+    return 0;
+}
+
+int display_dst(const char *caption)
+{
+    string filename = caption;
+    filename = filename + ".jpg";
+    imwrite(filename, dst);
+    return 0;
 }
