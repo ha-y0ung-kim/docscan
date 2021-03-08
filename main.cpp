4666e04fbd540de8fb152e97c843894edd069cc4
supermegaukulele <findamy@naver.com>
2021-03-08 17:23:48 +0100

    add threshold demo

--- main.cpp
+++ main.cpp
@@ -12,14 +12,25 @@ int MAX_TYPE = 3;
 int BLUR_VALUE = 0;
 int MAX_VALUE = 100;
 
+int THRESHOLD_VALUE = 0;
+int THRESHOLD_TYPE = 0;
+int const MAX_THR_VALUE = 255;
+int const MAX_THR_TYPE = 4;
+int const MAX_BINARY_VALUE = 255;
+
 Mat src;
 Mat dst;
+Mat src_bw;
 
 const char *window_name = "Blur Demo";
 const char *trackbar_type = "Type: \n 0: Homogeneous \n 1: Gaussian \n 2: median_blur \n 3: Bilateral_Blur";
 const char *trackbar_value = "Value";
 
-int display_dst(const char *caption);
+const char *window2_name = "Threshold Demo";
+const char *trackbar2_type = "Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted";
+const char *trackbar2_value = "Value";
+
+//int display_dst(const char *caption);
 
 static void Blur_Demo(int, void *)
 {
@@ -57,8 +68,22 @@ static void Blur_Demo(int, void *)
     imshow(window_name, dst);
 }
 
+static void Threshold_Demo(int, void *)
+{
+    dst = src_bw.clone();
+    /* 0: Binary
+     1: Binary Inverted
+     2: Threshold Truncated
+     3: Threshold to Zero
+     4: Threshold to Zero Inverted
+    */
+    threshold(src_bw, dst, THRESHOLD_VALUE, MAX_BINARY_VALUE, THRESHOLD_TYPE);
+    imshow(window2_name, dst);
+}
+
 int main(int argc, char **argv)
 {
+    // For blur
     namedWindow(window_name, WINDOW_AUTOSIZE);
     createTrackbar(trackbar_type, window_name, &BLUR_TYPE, MAX_TYPE, Blur_Demo);
     createTrackbar(trackbar_value, window_name, &BLUR_VALUE, MAX_VALUE, Blur_Demo);
@@ -72,6 +97,14 @@ int main(int argc, char **argv)
         printf(" Usage:\n %s [image_name-- default lena.jpg] \n", argv[0]);
         return EXIT_FAILURE;
     }
+
+    cvtColor(src, src_bw, COLOR_BGR2GRAY); //Convert image to gray
+
+    namedWindow(window2_name, WINDOW_AUTOSIZE);
+    createTrackbar(trackbar2_type, window2_name, &THRESHOLD_TYPE, MAX_THR_TYPE, Threshold_Demo);    // Create a Trackbar to choose type of Threshold
+    createTrackbar(trackbar2_value, window2_name, &THRESHOLD_VALUE, MAX_THR_VALUE, Threshold_Demo); // Create a Trackbar to choose Threshold value
+    Threshold_Demo(0, 0);
+
     Blur_Demo(0, 0);
     waitKey();
 
